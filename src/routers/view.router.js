5e19a65644b11bdcfd056c261cbdb2172cdd73bf
Si Hao <tangsihao1998@gmail.com>
2022-06-28 01:01:06 +0700

    init FE
    - structure FE.
    - add scss homepage.
    - add js homepage.

--- src/routers/view.router.js
+++ src/routers/view.router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => res.render("index"));
-router.get('/magic', (req, res) => res.render("magic"));
+router.get('/', (req, res) => res.render("pages/home"));
+router.get('/lich-truc-tiep', (req, res) => res.render("pages/livestreamSchedule"));
+router.get('/highlights', (req, res) => res.render("pages/highlights"));
+router.get('/admin/login', (req, res) => res.render("pages/login-admin"));
 router.get('/chat', (req, res) => res.render("chat"));
 
 module.exports = router;
