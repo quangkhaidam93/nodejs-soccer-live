d14e7447b906d03a361b590bc34797f636f5cff7
Si Hao <tangsihao1998@gmail.com>
2022-07-04 01:59:10 +0700

    add login for admin

--- src/routers/view.router.js
+++ src/routers/view.router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/', (req, res) => res.render("pages/home"));
 router.get('/lich-truc-tiep', (req, res) => res.render("pages/livestreamSchedule"));
 router.get('/highlights', (req, res) => res.render("pages/highlights"));
-router.get('/admin/login', (req, res) => res.render("pages/login-admin"));
+router.get('/admin/login', (req, res) => res.render("pages/loginAdmin"));
 router.get('/chat', (req, res) => res.render("chat"));
 
 module.exports = router;
