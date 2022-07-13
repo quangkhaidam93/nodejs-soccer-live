7a44a9becd9678953b80833762b86bca7b62a057
Si Hao <tangsihao1998@gmail.com>
2022-07-13 10:55:29 +0700

    add admin ui
    - add caster ui
    - add club ui

--- src/routers/view.router.js
+++ src/routers/view.router.js
@@ -10,6 +10,7 @@ router.get('/lich-truc-tiep', (req, res) => {
 router.get('/highlights', (req, res) => res.render("pages/highlights"));
 router.get('/admin/login', (req, res) => res.render("pages/loginAdmin"));
 router.get('/admin/quan-li-user', (req, res) => res.render("pages/userManagement"));
+router.get('/admin/quan-li-caster', (req, res) => res.render("pages/casterManagement"));
 router.get('/chat', (req, res) => res.render("chat"));
 router.get('/admin', (req, res) => {
   res.redirect("admin/login");
