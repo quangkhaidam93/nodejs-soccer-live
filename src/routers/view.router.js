047bc696c5df06f4c9942676e98821e95fbc9a79
Si Hao <tangsihao1998@gmail.com>
2022-07-13 12:07:19 +0700

    add admin ui

--- src/routers/view.router.js
+++ src/routers/view.router.js
@@ -13,6 +13,8 @@ router.get('/admin/quan-li-user', (req, res) => res.render("pages/userManagement
 router.get('/admin/quan-li-caster', (req, res) => res.render("pages/casterManagement"));
 router.get('/admin/quan-li-clb', (req, res) => res.render("pages/clubManagement"));
 router.get('/admin/quan-li-giai', (req, res) => res.render("pages/leagueManagement"));
+router.get('/admin/quan-li-phong', (req, res) => res.render("pages/roomManagement"));
+router.get('/admin/quan-li-vai-tro', (req, res) => res.render("pages/roleManagement"));
 router.get('/chat', (req, res) => res.render("chat"));
 router.get('/admin', (req, res) => {
   res.redirect("admin/login");
