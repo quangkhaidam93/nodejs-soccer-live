90b7ac0abe9fb666de46b7a9b756ef69fd6081bd
Si Hao <tangsihao1998@gmail.com>
2022-07-16 04:28:30 +0700

    add sign up to test admin signup

---
+++ src/views/js/signUp.js
@@ -0,0 +1,31 @@
+function checkUsernameValue(username) {
+  return !!username;
+}
+
+function checkPassword(password, confirmPW) {
+  if (password.length < 10 || !password) {
+    alert("Please input password");
+    return false;
+  }
+  if (confirmPW.length < 10 || !confirmPW) {
+    alert("Please input confirm password");
+    return false;
+  }
+  if (password !== confirmPW) {
+    alert("Password and confirm password is not match");
+    return false;
+  }
+  return true;
+}
+
+function submitSignUp() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const confirmPW = document.getElementById("confirm__password").value;
+  const nickname = document.getElementById("nickname").value;
+  if (checkUsernameValue(username) && checkPassword(password, confirmPW)) {
+    console.log('den voi daddy')
+    signup(username, password, nickname);
+  }
+  return
+}
