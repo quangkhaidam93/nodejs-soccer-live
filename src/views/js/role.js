9c80e4297fc47e6c7f1eeeeb9965bea7a696a0c7
Si Hao <tangsihao1998@gmail.com>
2022-07-21 11:24:10 +0700

    add role modal

--- src/views/js/role.js
+++ src/views/js/role.js
@@ -1,6 +1,14 @@
 async function createNewRoleSubmit() {
   const roleName = document.getElementById('roleName').value;
-  createNewLeague({ name: leagueName, image: imageUrl }).finally(() => {
-    getAllLeagues();
-  });
+  // createNewLeague({ name: leagueName, image: imageUrl }).finally(() => {
+  //   getAllLeagues();
+  // });
+}
+
+async function onDeleteRoleConfirm() {
+
+}
+
+async function updateRoleSubmit() {
+
 }
