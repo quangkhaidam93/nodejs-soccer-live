3de7c163edb7c7790a4d855493b2c76420b73c65
Si Hao <tangsihao1998@gmail.com>
2022-07-23 11:26:32 +0700

    add livestream schedule page

--- src/routers/view.router.js
+++ src/routers/view.router.js
@@ -3,13 +3,14 @@ const router = express.Router();
 
 router.get('/test', (req, res) => res.render("pages/test"));
 router.get('/', (req, res) => res.render("pages/home"));
+router.get('/highlights', (req, res) => res.render("pages/highlights"));
 router.get('/lich-truc-tiep', (req, res) => {
   res.render("pages/livestreamSchedule")
 });
 router.get('/:roomId', (req, res) => {
-  res.render("pages/livestreamSchedule")
+  res.render("pages/livestreamRoom")
 });
-router.get('/highlights', (req, res) => res.render("pages/highlights"));
+
 router.get('/admin/login', (req, res) => res.render("pages/loginAdmin"));
 router.get('/admin/sign-up', (req, res) => res.render("pages/signUpAdmin"));
 router.get('/admin/quan-li-user', (req, res) => res.render("pages/userManagement"));
