3c33d8ea88e85d28fa63d5a392ce67cfeda2129f
Si Hao <tangsihao1998@gmail.com>
2022-07-23 12:12:11 +0700

    update caster modal function

--- src/views/js/casters.js
+++ src/views/js/casters.js
@@ -1,5 +1,5 @@
 let listCaster = [];
-let selectedCasterId;
+let selectedCaster;
 
 async function uploadImageFile(image) {
   const imageUrl = await uploadImage(image);
@@ -11,7 +11,16 @@ async function createNewCasterSubmit() {
   const image = document.getElementById('casterAvatarImage').files[0];
   let imageUrl;
   if (image) imageUrl = await uploadImageFile();
-  createNewCaster({ fullName: casterName, avatar: imageUrl }).finally(() => {
+  createNewCaster({ fullName: casterName, avatar: imageUrl })
+  .then(res=> {
+    if (res.data.statusCode === 200) {
+      alert('Tạo caster thành công');
+    }
+  })
+  .catch(err => {
+    alert('Tạo caster thất bại, vui lòng thử lại.')
+  })
+  .finally(() => {
     getListCasterData();
   });
 }
@@ -35,9 +44,9 @@ async function getListCasterData() {
         <td class="each__data" > ${moment(c.createdAt).format('DD/MM/YYYY')} </td>
         <td class="each__data" > ${moment(c.updatedAt).format('DD/MM/YYYY')} </td>
         <td class="each__data">
-          <button class="action__btn update__btn" data-bs-toggle="modal" data-bs-target="#updateCasterModal" onclick="selectCaster(${c.id})" >u</button>
-          <button class="action__btn info__btn" data-bs-toggle="modal" data-bs-target="#infoCasterModal" onclick="selectCaster(${c.id})" >i</button>
-          <button class="action__btn delete__btn" data-bs-toggle="modal" data-bs-target="#deleteCasterModal" onclick="selectCaster(${c.id})" >d</button>
+          <button class="action__btn update__btn" data-bs-toggle="modal" data-bs-target="#updateCasterModal" onclick="selectCaster(${c.id}, 'update')" >u</button>
+          <button class="action__btn info__btn" data-bs-toggle="modal" data-bs-target="#infoCasterModal" onclick="selectCaster(${c.id}, 'info')" >i</button>
+          <button class="action__btn delete__btn" data-bs-toggle="modal" data-bs-target="#deleteCasterModal" onclick="selectCaster(${c.id}, 'delete')" >d</button>
         </td>
       </tr>`
   })
@@ -46,18 +55,54 @@ async function getListCasterData() {
 getListCasterData();
 
 // select caster
-function selectCaster(casterId) {
-  selectedCasterId = casterId;
+function selectCaster(casterId, type) {
+  const dataSelectedCaster = listCaster.find(c => c.id === casterId);
+  selectedCaster = dataSelectedCaster;
+  switch(type) {
+    case 'update': {
+      let casterName = document.getElementById('updateCasterName');
+      casterName.value = selectedCaster.fullName;
+      break;
+    }
+    case 'info': {
+      let casterName = document.getElementById('infoCasterName');
+      casterName.value = selectedCaster.fullName;
+      break;
+    }
+    case 'delete': {
+      let deleteText = document.getElementById('deleteBodyText');
+      var child = deleteText.lastElementChild; 
+      while (child) {
+        deleteText.removeChild(child);
+        child = deleteText.lastElementChild;
+      }
+      deleteText.innerHTML += `
+      <div>
+        Bạn có chắc chắn xóa caster
+        <span style="color: red; font-weight: bold"> ${selectedCaster.fullName} </span>
+        này không ?
+      </div>`
+      break;
+    }
+  }
 }
 
 // update caster
 async function updateCasterSubmit(casterId) {
   const casterName = document.getElementById('updateCasterName').value;
   const image = document.getElementById('casterAvatarImage').files[0];
-  const selectedCaster = listCaster.find(c => c.id === selectedCasterId);
-  let imageUrl;
+  let imageUrl = selectedCaster.avatar;
   if (image) imageUrl = await uploadImageFile(image);
-  updateCaster(selectedCasterId, { fullName: casterName, avatar: imageUrl }).finally(() => {
+  updateCaster(selectedCaster.id, { fullName: casterName, avatar: imageUrl })
+  .then(res => {
+    if (res.data.statusCode === 200) {
+      alert('Cập nhật caster thành công');
+    }
+  })
+  .catch(err => {
+    alert('Cập nhật caster thất bại, vui lòng thử lại');
+  })
+  .finally(() => {
     $('#updateCasterModal').modal('hide');
     getListCasterData();
   });
@@ -66,9 +111,18 @@ async function updateCasterSubmit(casterId) {
 // xóa caster
 async function onDeleteCasterConfirm(casterId) {
   try {
-    await deleteCaster(selectedCasterId);
-    $('#deleteCasterModal').modal('hide');
-    getListCasterData();
+    await deleteCaster(selectedCaster.id).then(res => {
+      if (response.data.statusCode === 200) {
+        alert('Xóa caster thành công')
+      }
+    })
+    .catch(err => {
+      alert('Xóa caster bị lỗi, vui lòng thử lại')
+    })
+    .finally(() => {
+      $('#deleteCasterModal').modal('hide');
+      getListCasterData();
+    });
   } catch (err) {
     console.log(err);
   }
