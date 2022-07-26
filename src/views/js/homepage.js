34bde9ea8ca57a005b00e5acc938f429323a5f8e
Si Hao <tangsihao1998@gmail.com>
2022-07-26 09:49:19 +0700

    add highlight streaming

---
+++ src/views/js/homepage.js
@@ -0,0 +1,18 @@
+let highlightRoom;
+let listRooms = [];
+
+
+async function getListRoomData() {
+  const fetchedRooms = await getAllRooms();
+  const listRoom = [...fetchedRooms];
+
+  highlightRoom = listRoom[0];
+  listRooms = listRoom;
+
+  const matchNewestInfo = document.getElementById("matchInfoNewest");
+  matchNewestInfo.innerHTML = `
+    <iframe class="livestream__video" src=${highlightRoom.streamUrl}></iframe>
+  `
+}
+
+getListRoomData();
